5f4b2ef46c0da9379fa890374205f35d5c4ff680
saipriyakandikeri <125527066+saipriyakandikeri@users.noreply.github.com>
2023-02-22 10:27:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,13 +42,4 @@ my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
 
-my_data_row = my_cur.fetchone()
-streamlit.header("The fruit load  list contains")
-streamlit.dataframe(my_data_row)
-
-my_cur.execute("select * from fruit load list")
-my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit load list contain")
-streamlit.dataframe(my_data_rows)
-
 

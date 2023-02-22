5483309fc8efa8dae11e395d57a62925db618353
saipriyakandikeri <125527066+saipriyakandikeri@users.noreply.github.com>
2023-02-22 15:18:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 #import streamlit
 #import pandas
-import requests
-import snowflake.connector
-from urllib.error import url error
+#import requests
+#import snowflake.connector
+
 
 streamlit.title('My Parents New Healthy Dinner')
 streamlit.header('Breakfast Favorites')
